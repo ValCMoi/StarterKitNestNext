0f8a9d05a0f9dd92fdd05211a5846585507489aa
ValCMoi <valentin.rouxel.pro@gmail.com>
2023-01-13 12:18:19 +0100

    ajout localAuthGuard

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from '@nestjs/passport/dist';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/guards/local-auth-guard';
 
 @Controller()
 export class AppController {
@@ -17,7 +18,7 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
